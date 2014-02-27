e419049bbeb6f69317187aad81bcdbcec6026efb
Shyam <shyam.salim.kumar@gmail.com>
2014-02-27 23:48:28 +0530

    - Applied some principles of DRY. (Converted factories into a single factory)  - Moved localStorage code into a service.

--- app/scripts/controller.js
+++ app/scripts/controller.js
@@ -1,86 +1,79 @@
 var storeControllers = angular.module('storeControllers', []);
 
-storeControllers.controller('menCtrl', ['$scope', 'men',
+storeControllers.controller('homeCtrl', [function( ) { }]);
+
+storeControllers.controller('menCtrl', ['$scope', 'dress',
   function($scope, men) {
   	 men.get(function(dresses){
     $scope.men = dresses.men;
   });
   }]);
 
-storeControllers.controller('womenCtrl', ['$scope', 'women',
+storeControllers.controller('womenCtrl', ['$scope', 'dress',
   function($scope, women) {
   	 women.get(function(dresses){
     $scope.women = dresses.women;
   });
   }]);
 
-storeControllers.controller('kidsCtrl', ['$scope', 'kids',
+storeControllers.controller('kidsCtrl', ['$scope', 'dress',
   function($scope, kids) {
   	 kids.get(function(dresses){
     $scope.kids = dresses.kids;
   });
   }]);
 
-storeControllers.controller('menDetailsCtrl', ['$scope', '$routeParams', '$http',
+storeControllers.controller('menDetailsCtrl', ['$scope', '$routeParams', 'dress',
   function($scope, $routeParams, mensDetails) {
-       mensDetails.get('json/dresses.json').success(function(data){
+       mensDetails.get(function(data){
        	$scope.mens = data.men[$routeParams.id];
-       	console.log($scope.mens);
-       
     });
 
   }]);
 
-storeControllers.controller('womenDetailsCtrl', ['$scope', '$routeParams', '$http',
+storeControllers.controller('womenDetailsCtrl', ['$scope', '$routeParams', 'dress',
   function($scope, $routeParams, womensDetails) {
-       womensDetails.get('json/dresses.json').success(function(data){
+       womensDetails.get(function(data){
        	$scope.womens = data.women[$routeParams.id];       
     });
 
   }]);
 
-storeControllers.controller('kidsDetailsCtrl', ['$scope', '$routeParams', '$http',
+storeControllers.controller('kidsDetailsCtrl', ['$scope', '$routeParams', 'dress',
   function($scope, $routeParams, kidsDetails) {
-       kidsDetails.get('json/dresses.json').success(function(data){
+       kidsDetails.get(function(data){
        	$scope.kids = data.kids[$routeParams.id];       
     });
 
   }]);
 
-storeControllers.controller('cartCtrl', function($scope) {
-	$scope.saved = localStorage.getItem('products');
-	$scope.products = (localStorage.getItem('products')!==null) ? JSON.parse($scope.saved) : [ ];
-	localStorage.setItem('products', JSON.stringify($scope.products));
-	$scope.addCart = function(img,desc,price,quantity) {
-         $scope.products.push({
-			image: img,
-			name: desc,
-			price: price,
+storeControllers.controller('cartCtrl', ['$scope', 'prodCart', function($scope, prodCart) {
+	$scope.saved = prodCart.getCart();
+	$scope.products = $scope.saved;
+	$scope.addCart = function( img, desc, price, quantity ) {
+    var item = {
+      image: img,
+      name: desc,
+      price: price,
+    };
+    $scope.products.push(item);
+    prodCart.addToCart( item );
+  };
 
-		});
-          localStorage.setItem('products', JSON.stringify($scope.products));
-    }
+}]);
 
-});
-
-storeControllers.controller('pdtCtrl', function($scope) {
-	if(localStorage.getItem("products") ===null)
-		  {
-		  	$scope.products = [];
-		  }
-		  else
-		  {
-		  	$scope.products = JSON.parse(localStorage.getItem("products")); // get value from localstorage
-		  }
-	  
-	  
-	  $scope.clear = function(index){ 
-		  //$scope.products = [];
-		  $scope.products.splice(index, 1);
-		 localStorage.setItem('products', JSON.stringify($scope.products));
-                  // clear all value in particular storage variable in HTML5 localstorage
-	};
-});
+storeControllers.controller('pdtCtrl', ['$scope', 'prodCart', function($scope, prodCart) {
+  if(prodCart.getCart() ===null) {
+    $scope.products = [];
+  } else {
+    $scope.products = prodCart.getCart(); // get value from localstorage
+  }
+  
+  $scope.clear = function () {
+    prodCart.clearCart();
+    $scope.products = prodCart.getCart();
+  };
+}]);
 storeControllers.controller('orderCtrl', function($scope, $location) {
 	
 	$scope.submit = function() {
